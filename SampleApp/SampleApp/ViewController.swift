237c1e58bf9470eaf1cd794b5236a9196f8707b1
Naresh <inareshbabumv@gmail.com>
2023-06-27 10:08:24 +0530

    Base Branch Commit

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Naresh on 27/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Main Branch")
+    }
+
+
+}
+
